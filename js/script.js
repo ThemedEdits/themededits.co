615cd1aebe553e41b79db5ddd29bfad2afc2f3d4
Themed <themed.edits.co@gmail.com>
2026-08-10 09:08:34 +0500

    fixed some

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const lenis = new Lenis({
   duration: 1.2,
   easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
   smoothWheel: true,
-  smoothTouch: false,
+  smoothTouch: true,
 });
 
 function raf(time) {
